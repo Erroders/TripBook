2d117e06065764459decad74a36c7ffed9800937
Sparsh Agarwal <sparshaga25@gmail.com>
2021-05-21 14:39:19 +0530

    Added Routing - Initialized HomeLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+
+import { HomeLayout } from './layouts/HomeLayout';
 
 const App: React.FC = () => {
     return (
         <>
-            <h1>Hello World!</h1>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/home" exact={true}>
+                        <HomeLayout />
+                    </Route>
+                    <Redirect from="/" to="/home" />
+                    <Route />
+                </Switch>
+            </BrowserRouter>
         </>
     );
 };
---
+++ src/layouts/HomeLayout.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const HomeLayout: React.FC = () => {
+    return <div>Home Layout</div>;
+};
