75c402faedbdde0fad41e7ba98d78f52353f2688
nonitmittal <nonitmittal@jklu.edu.in>
2021-06-11 12:54:40 +0530

    Resolved Bugs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const index: React.FC = () => {
     return (
-        <header className="flex bg-primary-gray h-header text-xl font-semibold font-display w-full sticky top-0 z-50">
-            <h1 className="m-auto">TripBook</h1>
+        <header className="flex bg-primary-gray h-header text-xl font-semibold font-display w-full sticky top-0 z-50 border-b border-gray-300 ">
+            <h1 className="m-auto tracking-wider">TripBook</h1>
         </header>
     );
 };
