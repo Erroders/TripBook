5df2a8777a3207c306697f20e3822d4e10f03f1b
Sparsh Agarwal <sparshaga25@gmail.com>
2021-06-13 21:58:16 +0530

    Fixed Bug: - Back button not working on Create Trip

--- src/views/AddView.tsx
+++ src/views/AddView.tsx
@@ -6,14 +6,20 @@ const AddView: React.FC = () => {
     const history = useHistory();
 
     useEffect(() => {
-        const user = 'akathecoder';
-        getCurrentTrip(user).then((currentTrip) => {
-            if (currentTrip) {
-                history.push(`/user/${user}/${currentTrip}`);
-            } else {
-                history.push('/createTrip');
-            }
-        });
+        console.log(history.action);
+
+        if (history.action == 'POP') {
+            history.goBack();
+        } else {
+            const user = 'akathecoder';
+            getCurrentTrip(user).then((currentTrip) => {
+                if (currentTrip) {
+                    history.push(`/user/${user}/${currentTrip}`);
+                } else {
+                    history.push('/createTrip');
+                }
+            });
+        }
     }, []);
 
     return <></>;
