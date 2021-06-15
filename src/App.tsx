3666bfe4caa44173441d4f0441e72cfbc375efe1
rG <12301raghavgoyal@gmail.com>
2021-06-15 13:27:01 +0530

    fix routes and add children to home layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,77 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import CreateTrip from './components/AddTrip/CreateTrip';
-import HomeLayout from './layouts/MainLayout';
-import EditTrip from './views/EditTrip';
-import Trip from './views/Trip';
 import LoadingContext from './contexts/LoadingContext';
 import LoginedUserContext from './contexts/LoginedUserContext';
+import UserProfile from './views/UserProfile';
+import HomeFeed from './views/HomeFeed';
+import AddView from './views/AddView';
+import Explore from './views/Explore';
+import Notifications from './views/Notifications';
+import Followers from './views/Followers';
+import Followings from './views/Followings';
+import EditTrip from './views/EditTrip';
+import Trip from './views/Trip';
 import { USER_DATA } from './models/UserData';
+import { auth } from './utils/firebase/firebase';
+import LoginPage from './layouts/LoginPage';
 
 const App: React.FC = () => {
     const [loading, setLoading] = useState<boolean>(true);
     const [user, _setUser] = useState<USER_DATA | null>(null);
 
+    useEffect(() => {
+        console.log('user', user);
+        const unsubscribe = auth.onAuthStateChanged((_user) => {
+            console.log(_user);
+        });
+        return unsubscribe;
+    }, [user]);
+
     return (
-        <>
-            <LoginedUserContext.Provider value={user}>
-                <LoadingContext.Provider value={{ loading, setLoading }}>
-                    <BrowserRouter>
-                        <Switch>
-                            <Route path={['/home', '/explore', '/add', '/notifications', '/profile']}>
-                                <HomeLayout />
-                            </Route>
-                            <Route path="/user/:userId/:tripId" exact={true}>
-                                <Trip />
-                            </Route>
-                            <Route path="/user/:userId/edit/:tripId/:index" exact={true}>
-                                <EditTrip />
-                            </Route>
-                            <Route path="/createTrip" exact={true}>
-                                <CreateTrip />
-                            </Route>
-                            <Redirect from="/" to="/home" />
-                            <Route />
-                        </Switch>
-                    </BrowserRouter>
-                </LoadingContext.Provider>
-            </LoginedUserContext.Provider>
-        </>
+        <LoginedUserContext.Provider value={user}>
+            <LoadingContext.Provider value={{ loading, setLoading }}>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path={'/login'}>
+                            <LoginPage />
+                        </Route>
+                        <Route path={'/home'}>
+                            <HomeFeed />
+                        </Route>
+                        <Route path={'/add'}>
+                            <AddView />
+                        </Route>
+                        <Route path={'/notifications'}>
+                            <Notifications />
+                        </Route>
+                        <Route path={'/explore'}>
+                            <Explore />
+                        </Route>
+                        <Route path="/user/:userId/:tripId" exact={true}>
+                            <Trip />
+                        </Route>
+                        <Route path="/user/:userId/edit/:tripId/:index" exact={true}>
+                            <EditTrip />
+                        </Route>
+                        <Route path="/createTrip" exact={true}>
+                            <CreateTrip />
+                        </Route>
+                        <Route path="/profile/" exact={true}>
+                            <UserProfile />
+                        </Route>
+                        <Route path="/profile/:userId/followers" exact={true}>
+                            <Followers />
+                        </Route>
+                        <Route path="/profile/:userId/followings" exact={true}>
+                            <Followings />
+                        </Route>
+                        <Redirect from="/" to="/home" />
+                        <Route />
+                    </Switch>
+                </BrowserRouter>
+            </LoadingContext.Provider>
+        </LoginedUserContext.Provider>
     );
 };
 
--- src/views/HomeFeed.tsx
+++ src/views/HomeFeed.tsx
@@ -2,43 +2,46 @@ import React from 'react';
 import TripBox from '../components/HomeFeed/TripBox';
 import { TRIP_DATA } from '../models/TripData';
 import { getTripData } from '../utils/getTripData';
+import HomeLayout from '../layouts/MainLayout';
 
 const HomeFeed: React.FC = () => {
     const trip: TRIP_DATA = getTripData('unicef', 'adasd');
 
     return (
-        <div className="pb-14">
-            <TripBox
-                id={'adasd'}
-                coverImage={trip.coverImage}
-                noOfPosts={trip.noOfPosts}
-                title={trip.title}
-                details={trip.details}
-                userProfilePhotoUrl={trip.userProfilePhotoUrl}
-                username={trip.username}
-                lastUpdated={trip.lastUpdated}
-            />
-            <TripBox
-                id={'adasd'}
-                coverImage={trip.coverImage}
-                noOfPosts={trip.noOfPosts}
-                title={trip.title}
-                details={trip.details}
-                userProfilePhotoUrl={trip.userProfilePhotoUrl}
-                username={trip.username}
-                lastUpdated={trip.lastUpdated}
-            />
-            <TripBox
-                id={'adasd'}
-                coverImage={trip.coverImage}
-                noOfPosts={trip.noOfPosts}
-                title={trip.title}
-                details={trip.details}
-                userProfilePhotoUrl={trip.userProfilePhotoUrl}
-                username={trip.username}
-                lastUpdated={trip.lastUpdated}
-            />
-        </div>
+        <HomeLayout>
+            <div className="pb-14">
+                <TripBox
+                    id={'adasd'}
+                    coverImage={trip.coverImage}
+                    noOfPosts={trip.noOfPosts}
+                    title={trip.title}
+                    details={trip.details}
+                    userProfilePhotoUrl={trip.userProfilePhotoUrl}
+                    username={trip.username}
+                    lastUpdated={trip.lastUpdated}
+                />
+                <TripBox
+                    id={'adasd'}
+                    coverImage={trip.coverImage}
+                    noOfPosts={trip.noOfPosts}
+                    title={trip.title}
+                    details={trip.details}
+                    userProfilePhotoUrl={trip.userProfilePhotoUrl}
+                    username={trip.username}
+                    lastUpdated={trip.lastUpdated}
+                />
+                <TripBox
+                    id={'adasd'}
+                    coverImage={trip.coverImage}
+                    noOfPosts={trip.noOfPosts}
+                    title={trip.title}
+                    details={trip.details}
+                    userProfilePhotoUrl={trip.userProfilePhotoUrl}
+                    username={trip.username}
+                    lastUpdated={trip.lastUpdated}
+                />
+            </div>
+        </HomeLayout>
     );
 };
 
